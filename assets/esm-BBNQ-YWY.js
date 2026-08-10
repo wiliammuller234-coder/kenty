13364a6a8a393f9393660503429642a34766a74a
kardo <_EMAIL_GITHUB>
2026-08-10 20:17:44 +0300

    Show real avatar photo in chat list

--- assets/esm-BBNQ-YWY.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-6ASEdzU2.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
---
+++ assets/esm-qBdypwTd.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-BSII1G03.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
