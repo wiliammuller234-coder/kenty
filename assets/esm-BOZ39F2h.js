771eccbdfb55d6f8f9be66e4277e77610a181c88
kardo <_EMAIL_GITHUB>
2026-08-10 20:37:28 +0300

    Fix stale friend emoji overriding live profile in Purchases

---
+++ assets/esm-BOZ39F2h.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-Br2ombw3.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
--- assets/esm-Cxe3JfRS.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-05hn4qYp.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
