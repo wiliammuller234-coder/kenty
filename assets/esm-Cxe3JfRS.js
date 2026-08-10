883f58a9562399d167bcb102eaf0b959cd8d3304
kardo <_EMAIL_GITHUB>
2026-08-10 20:19:17 +0300

    Show real avatar photo in chat header too

---
+++ assets/esm-Cxe3JfRS.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-05hn4qYp.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
--- assets/esm-qBdypwTd.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-BSII1G03.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
