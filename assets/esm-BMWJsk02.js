89a5aed4f360bd789db91a2fe4ae333763c29e3d
kardo <_EMAIL_GITHUB>
2026-08-11 19:34:57 +0300

    Allow removing fake local contacts

---
+++ assets/esm-BMWJsk02.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CnTWXIcd.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
--- assets/esm-DyQjtQuW.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-DwJwJp3R.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
