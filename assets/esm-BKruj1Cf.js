9fd9c48bc022a499ed468a6c0e6a8aada601e65f
kardo <_EMAIL_GITHUB>
2026-08-14 09:00:33 +0300

    Fix app crash when caller both watches and starts a call in the same chat

---
+++ assets/esm-BKruj1Cf.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-B9mhOdWp.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
--- assets/esm-DdrTRbZR.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-DGIyiD9G.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
