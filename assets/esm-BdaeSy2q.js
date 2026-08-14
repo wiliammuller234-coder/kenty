4b23d82a1b356453def89ad33ee526165d775f8c
kardo <_EMAIL_GITHUB>
2026-08-14 10:00:51 +0300

    Fix call not connecting after Accept: hand off the channel from the watcher to CallRoom

---
+++ assets/esm-BdaeSy2q.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-BaNgUDMD.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
--- assets/esm-BouQ8oZP.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-CKpz45kA.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
