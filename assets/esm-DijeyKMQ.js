1bb6bbf155807c2d1f0eb6e9f809a3641fcf8699
kardo <_EMAIL_GITHUB>
2026-08-14 11:25:58 +0300

    Allow enabling camera mid-call (even on audio calls) and switching front/back camera

---
+++ assets/esm-DijeyKMQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-C1Rl120E.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
--- assets/esm-DsOZXeha.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-Qs4QlvoA.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
