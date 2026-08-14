5492554e104780224bc5737e754608ae5b7ee6f8
kardo <_EMAIL_GITHUB>
2026-08-14 09:16:56 +0300

    Show incoming-call banner even while on the Chat tab

--- assets/esm-BV7Mzpye.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-BSkhCzfp.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
---
+++ assets/esm-DEB86ObV.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-BJBdELoQ.js";var t=e(`PushNotifications`,{});export{t as PushNotifications};
